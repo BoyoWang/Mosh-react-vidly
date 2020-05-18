955cd7ab96da02e24beda218a349bb237c04e47e
Poyao Wang <boyomac@Poyaos-MacBook-Pro.local>
2020-05-18 20:17:41 +0900

    genreSelect display made

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -10,6 +10,7 @@ class Movies extends Component {
   state = {
     movies: [],
     genres: [],
+    selectedGenre: {},
     pageSize: 4,
     currentPage: 1,
   };
@@ -36,7 +37,7 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    console.log(genre);
+    this.setState({ selectedGenre: genre });
   };
 
   listItem = (movie) => {
@@ -86,6 +87,7 @@ class Movies extends Component {
         <div className="col-3">
           <ListGroup
             items={this.state.genres}
+            selectedItem={this.state.selectedGenre}
             onItemSelect={this.handleGenreSelect}
           />
         </div>
