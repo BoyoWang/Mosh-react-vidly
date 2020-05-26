edc5124662aed2a829f905800e1675076402c205
Poyao Wang <boyomac@Poyaos-MacBook-Pro.local>
2020-05-26 22:32:41 +0900

    10 A Basic Validation Implementation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -8,14 +8,26 @@ class LoginFrom extends Component {
   };
 
   validate = () => {
-    return { username: "Username is required." };
+    const errors = {};
+
+    const { account } = this.state;
+
+    if (account.username.trim() === "")
+      errors.username = "Username is required.";
+    if (account.password.trim() === "")
+      errors.password = "Password is required.";
+
+    return Object.keys(errors).length === 0 ? null : errors;
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
 
     const errors = this.validate();
+    console.log(errors);
     this.setState({ errors });
+
+    if (errors) return;
 
     //call the sever
     console.log("Submitted");
