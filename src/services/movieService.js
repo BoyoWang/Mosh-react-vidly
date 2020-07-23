eca2d67dc156b518415e175cdded316c057663a8
Poyao Wang <boyomac@MacBook-Pro.local>
2020-07-23 17:03:26 +0900

    28 Populating the Form

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -11,21 +11,8 @@ export const deleteMovie = (id) => {
   return http.delete(apiEndpoint + "/" + id);
 };
 
-// export function getMovie(id) {
-//   return movies.find(m => m._id === id);
-// }
+export function getMovie(id) {
+  return http.get(apiEndpoint + "/" + id);
+}
 
-// export function saveMovie(movie) {
-//   let movieInDb = movies.find(m => m._id === movie._id) || {};
-//   movieInDb.title = movie.title;
-//   movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-//   movieInDb.numberInStock = movie.numberInStock;
-//   movieInDb.dailyRentalRate = movie.dailyRentalRate;
-
-//   if (!movieInDb._id) {
-//     movieInDb._id = Date.now().toString();
-//     movies.push(movieInDb);
-//   }
-
-//   return movieInDb;
-// }
+export function saveMovie(movie) {}
